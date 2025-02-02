919c515d9439ffc41ff571873a5fbadaf078c2cf
Anuradha Bhaskar <anuradha.t.bhaskar@gmail.com>
2025-02-02 19:09:15 +0530

    implement user account retrieval and display in dashboard; add AccountCard component

--- app/(main)/dashboard/page.jsx
+++ app/(main)/dashboard/page.jsx
@@ -1,9 +1,12 @@
+import { getUserAccounts } from '@/actions/dashboard';
 import CreateAccountDrawer from '@/components/create-acccount-drawer';
 import { Card, CardContent } from '@/components/ui/card';
 import { Plus } from 'lucide-react';
 import React from 'react'
+import AccountCard from './_components/account-card';
 
-function DashboardPage(){
+async function DashboardPage(){
+  const accounts = await getUserAccounts();
   return (
     <div className='px-5'>
         {/* Budget Progress */}
@@ -13,11 +16,15 @@ function DashboardPage(){
           <CreateAccountDrawer>
             <Card className='hover:shadow-md transition-shadow cursor-pointer border-dotted'>
               <CardContent className='flex flex-col items-center justify-center text-muted-foreground h-full pt-5'>
-                <Plus className='h-10 w-10 mb-2'/>
-                <p className='text-sm font-medium'>Add New Account</p>
+                <Plus className='h-10 w-10 mb-2 text-gray-700'/>
+                <p className='text-sm font-medium text-gray-700'>Add New Account</p>
               </CardContent>
             </Card>
           </CreateAccountDrawer>
+          {/* Display accounts */}
+          {accounts.length > 0 && accounts?.map((account) => {
+            return <AccountCard key={account.id} account={account} />
+          })}
         </div>
     </div>
 
